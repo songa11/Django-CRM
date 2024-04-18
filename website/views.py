1db69a0b3bdd0f37a76735182a86d4bac8a0d2b2
songa11 <songazay@gmail.com>
2024-04-18 17:59:30 +0200

    Final Project

--- website/views.py
+++ website/views.py
@@ -1,3 +1,102 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
-# Create your views here.
+
+def home(request):
+    records = Record.objects.all()
+
+
+	#check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+             login(request, user)
+             messages.success(request, "You Have Been Logged In!")
+             return redirect('home')
+        else:
+             messages.success(request, "There Was An Error Logging In, Please Try Again ...")
+             return redirect('home')
+    else:
+        return render(request, 'home.html', {'records':records})
+    
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, "You Have Been Logged Out...")
+	return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()  # Always initialize the form variable
+    
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+        else:
+            # If form is invalid, continue rendering the register.html template
+            # with the form containing validation errors
+            return render(request, 'register.html', {'form': form})
+
+    # If request method is not 'POST', render the register.html template
+    # with the form to display the registration form
+    return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+     if request.user.is_authenticated:
+          # Look up Records
+          customer_record = Record.objects.get(id=pk)
+          return render(request, 'record.html', {'customer_record': customer_record})
+     else:
+          messages.success(request, "You Must Be Logged In ...")
+          return redirect('home')
+
+def delete_record(request, pk):
+     if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Successfully Deleted ...")
+        return redirect('home')
+     else:
+          messages.success(request, "You Must Be Logged In ...")
+          return redirect('home')
+     
+
+def add_record(request):
+     form = AddRecordForm(request.POST or None)
+     if request.user.is_authenticated:
+          if request.method == "POST":
+               add_record = form.save()
+               messages.success(request, "Record Added ...")
+               return redirect('home')
+          return render(request, 'add_record.html', {'form':form})
+     else:
+          messages.success(request, "You Must Be Logged In ...")
+          return redirect('home')
+     
+def update_record(request, pk):
+      if request.user.is_authenticated:
+           current_record = Record.objects.get(id=pk)
+           form = AddRecordForm(request.POST or None, instance=current_record)
+           if form.is_valid():
+                form.save()
+                messages.success(request, "Record Has Been Updated ...")
+                return redirect('home')
+           return render(request, 'update_record.html', {'form':form})
+      else:
+           messages.success(request, "You Must Be Logged In ...")
+           return redirect('home')
+           
